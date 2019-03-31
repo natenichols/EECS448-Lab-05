1ffec5fe57fb0ad89a7f88ef2bbf4eadc62504ca
Nathan N <nnichols@ku.edu>
2019-03-31 17:19:48 -0500

    initalizedfiles

--- CreateUser.php
+++ CreateUser.php
@@ -2,27 +2,27 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 echo <p>"entered"</p>;
-// $mysqli = new mysqli("mysql.eecs.ku.edu", "n545n545", "aehee7oh", "n545n545");
-// $username = $_POST["user"];
-// /* check connection */
-// if ($mysqli->connect_errno) {
-//     printf("Connect failed: %s\n", $mysqli->connect_error);
-//     exit();
-// }
-//
+$mysqli = new mysqli("mysql.eecs.ku.edu", "n545n545", "aehee7oh", "n545n545");
+$username = $_POST["user"];
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
 $query = "INSERT INTO Users (user_id) VALUES (username)";
-//
-// if ($result = $mysqli->query($query)) {
-//
-//     /* fetch associative array */
-//     while ($row = $result->fetch_assoc()) {
-//         printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
-//     }
-//
-//     /* free result set */
-//     $result->free();
-// }
-//
-// /* close connection */
-// $mysqli->close();
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+/* close connection */
+$mysqli->close();
 ?>
