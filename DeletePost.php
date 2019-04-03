912f64ff5650c3157f4ec6c907e01d725686e059
Nathan N <nnichols@ku.edu>
2019-04-03 13:23:36 -0500

    Update

--- DeletePost.php
+++ DeletePost.php
@@ -1,37 +1,27 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-
 $mysqli = new mysqli("mysql.eecs.ku.edu", "n545n545", "aehee7oh", "n545n545");
-
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     echo "<p>Could not connect to database</p>";
     exit();
 }
-
-
 $author = $_POST['choice'];
-
 $query = "SELECT content FROM Posts WHERE author_id = '$author'";
 $mysqli->query($query);
-
 if ($result = $mysqli->query($query)) {
-
     echo "<th> Posts from ".$author."</th>";
-
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["content"] . "</td>";
         echo "<tr>";
     }
-
     /* free result set */
     //$result->free();
 }
-
 /* close connection */
 $mysqli->close();
 ?>
--- ViewAllPosts.php
+++ ViewAllPosts.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "n545n545", "aehee7oh", "n545n545");
-echo "<p>Hello</p>";
+
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -16,23 +16,18 @@ $mysqli->query($query);
 
 if ($result = $mysqli->query($query)) {
 
-
-
     /* fetch associative array */
+
     while ($row = $result->fetch_assoc()) {
       echo "<tr>";
-      echo "<td><input value='" $row["post_id"] "'></input></td>";
+      echo "<td><input type = 'checkbox' value='" . $row["post_id"] . "'></input></td>";
       echo "<td>".$row["content"]."</td>";
-      echo "<td>".$row["author_id"]."</td>"
+      echo "<td>".$row["author_id"]."</td>";
       echo "</tr>";
     }
-
     /* free result set */
     //$result->free();
 }
-
-
-
 /* close connection */
 $mysqli->close();
 ?>
