116b7f6ac105a06fc11d46c00634df2c048b9e1b
Nathan N <nnichols@ku.edu>
2019-04-03 09:56:25 -0500

    ViewAllPosts

--- ViewAllPosts.php
+++ ViewAllPosts.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "n545n545", "aehee7oh", "n545n545");
-
+echo "<p>Hello</p>";
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -11,26 +11,27 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-
-$author = $_POST['choice'];
-
-$query = "SELECT content FROM Posts WHERE author_id = '$author'";
+$query = "SELECT author_id, content, post_id FROM Posts";
 $mysqli->query($query);
 
 if ($result = $mysqli->query($query)) {
 
-    echo "<th> Posts from ".$author."</th>";
+
 
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["content"] . "</td>";
-        echo "<tr>";
+      echo "<tr>";
+      echo "<td><input value='" $row["post_id"] "'></input></td>";
+      echo "<td>".$row["content"]."</td>";
+      echo "<td>".$row["author_id"]."</td>"
+      echo "</tr>";
     }
 
     /* free result set */
     //$result->free();
 }
+
+
 
 /* close connection */
 $mysqli->close();
